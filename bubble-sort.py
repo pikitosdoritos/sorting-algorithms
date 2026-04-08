6617a6c8ad8c0df7566494f3dcc9b957ebe49061
pikitosdoritos <fastovetsn@gmail.com>
2026-04-08 17:37:30 +0300

    Refactor bubble sort implementation to improve sorting logic and track attempts

--- bubble-sort.py
+++ bubble-sort.py
@@ -5,14 +5,21 @@ print("==" * 80)
 
 array = [2, 4, 1, 98, 34, 3, 88]
 
-
 def sort(array):
-    for _ in range(len(array)):
+    attempts = 0
+    for j in range(len(array)):
+        is_sorted = True
+        attempts += 1
         for i, item in enumerate(array):
-            if i < len(array) - 1 and item > array[i+1]:
+            if i < len(array) - 1 - j and item > array[i+1]:
                 array[i] = array[i + 1]
                 array[i + 1] = item
-
+                is_sorted = False
+                 
+        if is_sorted:
+            break
+        
+    print(attempts)
     return array
 
 print(sort(array))
