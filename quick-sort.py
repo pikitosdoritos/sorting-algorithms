40ac5ccdd3acf7dae746cf8b623802df3bd06caf
pikitosdoritos <fastovetsn@gmail.com>
2026-04-13 16:27:31 +0300

    Refactor quick sort implementation to improve variable naming and enhance readability

--- quick-sort.py
+++ quick-sort.py
@@ -1,16 +1,28 @@
 array = [2, 4, 1, 98, 34, 3, 88]
 
-def quick_sort(array, i=0, j=len(array) - 1):
+def quick_sort(array, start=0, end=len(array) - 1):
+    i = start
+    j = end
+    back = True
+    
     while i < j:
         if array[i] > array[j]:
             x = array[i]
             array[i] = array[j]
             array[j] = x
-            i += 1
+            back = not back
             
         else:
-            j -= 1
+            if back:
+                j -= 1
+            else:
+                i += 1
             
+    if i - start > 1:    
+        quick_sort(array, start, i-1)
+    if end - i > 1:
+        quick_sort(array, i+1, end)
+
     return array
     
 print(quick_sort(array))
