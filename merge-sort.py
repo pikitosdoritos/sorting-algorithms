8b3332e9ce19203efed98c6061e1fc17be354de0
pikitosdoritos <fastovetsn@gmail.com>
2026-04-14 22:36:44 +0300

    Implement merge sort function and update array definition

--- merge-sort.py
+++ merge-sort.py
@@ -1,1 +1,10 @@
 array = [1, 5, 9, 3, 6, 2, 4, 8, 7, 0]
+
+def merge_sort(array):
+    mid_point = len(array) // 2
+    arr1 = array[:mid_point]
+    arr2 = array[mid_point:]
+    
+    return arr1, arr2
+
+print(merge_sort(array))
