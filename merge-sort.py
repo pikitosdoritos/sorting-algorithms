dc31d14f5d51e94644111043513a3fd4979a2df3
pikitosdoritos <fastovetsn@gmail.com>
2026-04-14 22:49:40 +0300

    Refactor merge sort implementation to correctly return sorted array and improve readability

--- merge-sort.py
+++ merge-sort.py
@@ -1,10 +1,33 @@
 array = [1, 5, 9, 3, 6, 2, 4, 8, 7, 0]
 
+
 def merge_sort(array):
-    mid_point = len(array) // 2
-    arr1 = array[:mid_point]
-    arr2 = array[mid_point:]
+    if len(array) < 2:
+        return array
     
-    return arr1, arr2
+    mid_point = len(array) // 2
+    head = array[:mid_point]
+    tail = array[mid_point:]
+    
+    merge_sort(head)
+    merge_sort(tail)              
+    
+    result = []
+    
+    while True:
+        if len(tail) == 0:
+            result.append(*head)
+            break
+        
+        if len(head) == 0:
+            result.append(*tail)
+            break
+        
+        if head[0] > tail[0]:
+            result.append(tail.pop(0))
+        elif head[0] < tail[0]:
+            result.append(head.pop(0))
+        
+    return head, tail
 
 print(merge_sort(array))
