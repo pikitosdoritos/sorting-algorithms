e8dcb40d6c01d5d004771a232c426da7e84d3a92
pikitosdoritos <fastovetsn@gmail.com>
2026-04-16 22:43:47 +0300

    Implement selection sort algorithm

--- selection-sort.py
+++ selection-sort.py
@@ -1,4 +1,23 @@
 array = [3, 0, 1, 6, 9, 2, 4, 8, 5, 7]
 
 def selection_sort(arr):
-    pass
+    k = 0
+    
+    while k < len(arr) - 1:
+        i = k 
+        j = k + 1
+        
+        while j < len(arr):
+            if  arr[j] < arr[i]:
+                i = j
+                
+            j += 1
+                           
+            
+        arr[k], arr[i] = arr[i], arr[k]
+        
+        k += 1
+    
+    return arr
+
+print(selection_sort(array))           
